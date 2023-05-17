ba7e879b360fc2d8d40b4ee4931ac22828dd3a4e
worldsgreatestarcher <102760189+worldsgreatestarcher@users.noreply.github.com>
2023-05-16 18:54:03 -0700

    Add files via upload

---
+++ lab_7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int bits;
+    int bytes;
+    printf("How many bits? ");
+    scanf("%d", &bits);
+    printf("Page size in bytes? ");
+    scanf("%d", &bytes);
+}
